4c78073a04f85b63f4c28e05405b7a1cc3ffa36a
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-03 18:00:19 -0400

    enviando createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  if (typeof nome !== 'string') {
+    return undefined;
+  }
+  const objeto = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+
+  return objeto;
+};
 
 module.exports = createStudent;
