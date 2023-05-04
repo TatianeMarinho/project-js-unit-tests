b3ab5058ba8163fe9551888b2f765d0674a921f1
TatianeMarinho <devtatianemarinho@gmail.com>
2023-05-03 21:52:10 -0400

    enviando o productDetails sem finalizar

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,56 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
+   // ESCREVA SEUS TESTES ABAIXO:
+   // Teste se productDetails é uma função.
+  it('Teste se productDetails é uma função', () => {
+    expect(typeof productDetails).toBe('function');
+  });  
     // Teste se o retorno da função é um array.
+  it('Teste se o retorno da função é um array', () => {
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBeTruthy();
+  })  
     // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('Teste se array retornado pela função contém dois itens', () => {
+     expect(productDetails('Alcool gel', 'Máscara')).toHaveLength(2); 
   });
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
+  it('Teste se os dois itens dentro do array retornado pela função são objetos', () => {
+    expect(productDetails('plumas', 'pijamas')).toStrictEqual([
+      {
+        name: 'plumas',
+        details: {
+          productId: 'plumas123',
+        }
+      },
+      {
+        name: 'pijamas',
+        details: {
+          productId: 'pijamas123'
+        }
+      }
+    ]);
+  });
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si', () => {
+    expect(productDetails('macarrão', 'leite')).toStrictEqual([
+      {
+        name: 'macarrão',
+        details: {
+          productId: 'macarrão123',
+        }
+      },
+      {
+        name: 'leite',
+        details: {
+          productId: 'leite123'
+        }
+      }
+    ]);
+  });
+    // Teste se os dois productIds terminam com 123.
+    it('Teste se os dois productIds terminam com 123', () => {
+      expect(productDetails.details[0]).toMatch('123');
+    });
+  
 });
